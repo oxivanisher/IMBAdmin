06ab379fcadc0b2c1e2d9c68f93610aa1feec135
unknown <sampit@.(none)>
2011-02-25 23:26:16 +0100

    ImbaUserRole gehinzugeadded.

--- Constants.php
+++ Constants.php
@@ -18,6 +18,7 @@ class ImbaConstants {
  	*/
 	public static $DATABASE_TABLES_WOW_ARMORY_CHARCACHE = "oom_openid_armory_charcache";
 	public static $DATABASE_TABLES_SYS_USER_PROFILES = "oom_openid_user_profiles";
+	public static $DATABASE_TABLES_SYS_PROFILES = "oom_openid_profiles";
 	// TODO: Tabellen ergänzen
 	/*
  	oom_openid_armory_itemcache
@@ -30,7 +31,6 @@ class ImbaConstants {
  	oom_openid_messages
  	oom_openid_multig_games
  	oom_openid_multig_names
- 	oom_openid_profiles
  	oom_openid_request_log
  	oom_openid_session
  	oom_openid_settings
--- Model/ImbaUserRole.php
+++ Model/ImbaUserRole.php
@@ -8,8 +8,99 @@ class ImbaUserRole extends ImbaBase {
 	/**
  	* Fields
  	*/
- 	protected $handle;
+	protected $handle;
 	protected $role;
-		name	smf	eqdkp	wordpress	icon	phpraider 	
+	protected $name;
+	protected $smf;
+	protected $eqdkp;
+	protected $wordpress;
+	protected $icon;
+	protected $phpraider;
+
+	/**
+ 	* Properties
+ 	*/
+	public function getHandle() {
+		return $this -> handle;
+	}
+
+	public function setHandle($handle) {
+		$this -> handle = $handle;
+	}
+
+	public function getRole() {
+		return $this -> role;
+	}
+
+	public function setRole($role) {
+		$this -> role = $role;
+	}
+
+	public function getName() {
+		return $this -> name;
+	}
+
+	public function setName($name) {
+		$this -> name = $name;
+	}
+
+	public function getSmf() {
+		return $this -> smf;
+	}
+
+	public function setSmf($smf) {
+		$this -> smf = $smf;
+	}
+
+	public function getEqdkp() {
+		return $this -> eqdkp;
+	}
+
+	public function setEqdkp($eqdkp) {
+		$this -> eqdkp = $eqdkp;
+	}
+
+	public function getWordpress() {
+		return $this -> wordpress;
+	}
+
+	public function setWordpress($wordpress) {
+		$this -> wordpress = $wordpress;
+	}
+
+	public function getIcon() {
+		return $this -> icon;
+	}
+
+	public function setIcon($icon) {
+		$this -> icon = $icon;
+	}
+
+	public function getPhpraider() {
+		return $this -> phpraider;
+	}
+
+	public function setPhpraider($phpraider) {
+		$this -> phpraider = $phpraider;
+	}
+
+	/**
+ 	* Methods
+ 	*/
+	public function loadById($imbaDatabaseManager, $id) {
+		$query = "SELECT * FROM  " . ImbaConstants::$DATABASE_TABLES_SYS_PROFILES . " Where id = '" . $id . "';";
+		$imbaDatabaseManager -> query($query);
+		$result = $imbaDatabaseManager -> fetchRow();
+
+		$this -> setId($id);
+		$this -> setHandle($result["handle"]);
+		$this -> setRole($result["role"]);
+		$this -> setName($result["name"]);
+		$this -> setSmf($result["smf"]);
+		$this -> setEqdkp($result["eqdkp"]);
+		$this -> setWordpress($result["wordpress"]);
+		$this -> setIcon($result["icon"]);
+		$this -> setPhpraider($result["phpraider"]);
+	}
 }
 ?>

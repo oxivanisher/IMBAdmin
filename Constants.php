b11efa59214cfcfb592cede34cf8ddaba8be4973
Marc Urben <aegnor@mittelerde.ch>
2011-02-25 23:46:10 +0100

    monkey work done

--- Constants.php
+++ Constants.php
@@ -16,29 +16,30 @@ class ImbaConstants {
 	/**
  	* Database - Tables
  	*/
-	public static $DATABASE_TABLES_WOW_ARMORY_CHARCACHE = "oom_openid_armory_charcache";
+	public static $DATABASE_TABLES_SYS_PROFILES = "oom_openid_profiles";
+	public static $DATABASE_TABLES_SYS_REQUEST_LOG = "oom_openid_request_log";
+	public static $DATABASE_TABLES_SYS_SESSION = "oom_openid_session";
+	public static $DATABASE_TABLES_SYS_SETTINGS = "oom_openid_settings";
+	public static $DATABASE_TABLES_SYS_SYSTEMMESSAGES = "oom_openid_systemmessages";
+	public static $DATABASE_TABLES_SYS_ADMINUSERS = "oom_openid_usermanager";
+	public static $DATABASE_TABLES_SYS_FRONTEND_SAFE = "oom_openid_frontend_safe";
+	public static $DATABASE_TABLES_SYS_MULTIG_GAMES = "oom_openid_multig_games";
 	public static $DATABASE_TABLES_SYS_USER_PROFILES = "oom_openid_user_profiles";
-	// TODO: Tabellen ergänzen
-	/*
- 	oom_openid_armory_itemcache
- 	oom_openid_armory_names
- 	oom_openid_characters
- 	oom_openid_chatchannels
- 	oom_openid_chatmessages
- 	oom_openid_frontend_safe
- 	oom_openid_lastonline
- 	oom_openid_messages
- 	oom_openid_multig_games
- 	oom_openid_multig_names
- 	oom_openid_profiles
- 	oom_openid_request_log
- 	oom_openid_session
- 	oom_openid_settings
- 	oom_openid_systemmessages
- 	oom_openid_usermanager
- 	oom_openid_user_applications	
- 	oom_openid_xmpp
- 	*/
+
+	public static $DATABASE_TABLES_USR_MESSAGES = "oom_openid_messages";
+	public static $DATABASE_TABLES_USR_MULTIG_NAMES = "oom_openid_multig_names";
+	public static $DATABASE_TABLES_USR_APPLICATIONS = "oom_openid_user_applications";
+	public static $DATABASE_TABLES_USR_XMPP = "oom_openid_xmpp";
+	public static $DATABASE_TABLES_USR_LASTONLINE = "oom_openid_lastonline";
+
+	public static $DATABASE_TABLES_WOW_armory_itemcache = "oom_openid_armory_itemcache";
+	public static $DATABASE_TABLES_WOW_armory_names = "oom_openid_armory_names";
+	public static $DATABASE_TABLES_WOW_CHARACTERS = "oom_openid_characters";
+	public static $DATABASE_TABLES_WOW_ARMORY_CHARCACHE = "oom_openid_armory_charcache";
+
+	public static $DATABASE_TABLES_CHAT_CHATCHANNELS = "oom_openid_chatchannels";
+	public static $DATABASE_TABLES_CHAT_CHATMESSAGES = "oom_openid_chatmessages";
+
 }
 
 ?>
--- Model/ImbaUser.php
+++ Model/ImbaUser.php
@@ -14,6 +14,8 @@ class ImbaUser extends ImbaBase {
 	protected $firstname;
 	protected $lastname;
 	protected $birthday;
+	protected $birthmonth;
+	protected $birthyear;
 	protected $sex;
 	protected $icq;
 	protected $msn;
@@ -25,7 +27,7 @@ class ImbaUser extends ImbaBase {
 	protected $motto;
 	protected $accurate;
 	protected $role;
-	protected $armorychars;
+	// deleted protected $armorychars; oxi, 25.02.2011
 
 	/**
  	* Properties
@@ -46,6 +48,142 @@ class ImbaUser extends ImbaBase {
 		$this -> nickname = $nickname;
 	}
 
+	public function getEmail() {
+		return $this -> email;
+	}
+
+	public function setEmail($email) {
+		$this -> email = $email;
+	}
+
+	public function getFirstname() {
+		return $this -> $firstname;
+	}
+
+	public function setFirstname($firstname) {
+		$this -> $firstname = $firstname;
+	}
+
+	public function getLastname() {
+		return $this -> lastname;
+	}
+
+	public function setLastname($lastname) {
+		$this -> lastname = $lastname;
+	}
+
+	public function getBirthday() {
+		return $this -> birthday;
+	}
+
+	public function setBirthday($birthday) {
+		$this -> birthday = $birthday;
+	}
+
+	public function getBirthmonth() {
+		return $this -> birthmonth;
+	}
+
+	public function setBirthmonth($birthmonth) {
+		$this -> birthmonth = $birthmonth;
+	}
+
+	public function getBirthyear() {
+		return $this -> birthyear;
+	}
+
+	public function setBirthyear($birthyear) {
+		$this -> birthyear = $birthyear;
+	}
+
+	public function getSex() {
+		return $this -> sex;
+	}
+
+	public function setSex($sex) {
+		$this -> sex = $sex;
+	}
+
+	public function getIcq() {
+		return $this -> icq;
+	}
+
+	public function setIcq($icq) {
+		$this -> icq = $icq;
+	}
+
+	public function getMsn() {
+		return $this -> msn;
+	}
+
+	public function setMsn($msn) {
+		$this -> msn = $msn;
+	}
+
+	public function getSkype() {
+		return $this -> skype;
+	}
+
+	public function setSkype($skype) {
+		$this -> skype = $skype;
+	}
+
+	public function getUsertitle() {
+		return $this -> usertitle;
+	}
+
+	public function setUsertitle($usertitle) {
+		$this -> usertitle = $usertitle;
+	}
+
+	public function getAvatar() {
+		return $this -> avatar;
+	}
+
+	public function setAvatar($avatar) {
+		$this -> avatar = $avatar;
+	}
+
+	public function getSignature() {
+		return $this -> signature;
+	}
+
+	public function setSignature($signature) {
+		$this -> signature = $signature;
+	}
+
+	public function getWebsite() {
+		return $this -> website;
+	}
+
+	public function setWebsite($website) {
+		$this -> website = $website;
+	}
+
+	public function getMotto() {
+		return $this -> motto;
+	}
+
+	public function setMotto($motto) {
+		$this -> motto = $motto;
+	}
+
+	public function getRole() {
+		return $this -> role;
+	}
+
+	public function setRole($role) {
+		$this -> role = $role;
+	}
+
+	public function getAccurate() {
+		return $this -> accurate;
+	}
+
+	public function setAccurate($accurate) {
+		$this -> accurate = $accurate;
+	}
+
 	// TODO: Setter und Getter der Fields ergänzen
 
 	/**
@@ -58,7 +196,23 @@ class ImbaUser extends ImbaBase {
 
 		$this -> openId = $openId;
 		$this -> nickname = $result["nickname"];
-		// TODO: Setzen der Felder ergänzen
+		$this -> email = $result["email"];
+		$this -> firstname = $result["firstname"];
+		$this -> lastname = $result["lastname"];
+		$this -> birthday = $result["birthday"];
+		$this -> birthmonth = $result["birthmonth"];
+		$this -> birthyear = $result["birthyear"];
+		$this -> sex = $result["sex"];
+		$this -> icq = $result["icq"];
+		$this -> msn = $result["msn"];
+		$this -> skype = $result["skype"];
+		$this -> usertitle = $result["usertitle"];
+		$this -> avatar = $result["avatar"];
+		$this -> signature = $result["signature"];
+		$this -> website = $result["website"];
+		$this -> motto = $result["motto"];
+		$this -> accurate = $result["accurate"];
+		$this -> role = $result["role"];
 	}
 
 }

2309b7fb98617673d809f527e312cea50a552eb1
Aggravate <>
2011-02-26 14:19:59 +0100

    SharedFunctions hinzugefügt.

---
+++ Controller/SharedFunctions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Collection of Functions
+ */
+class SharedFunctions {
+
+    // TODO: Cernus Methoden hier rein packen als !!!static!!!
+    public static function CernuMethod() {
+        return "do that thing do it!";
+    }
+
+}
+
+?>

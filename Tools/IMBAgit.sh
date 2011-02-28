68906ab734991d4f8126e34189b23507ef95f0b6
Marc Urben <aegnor@mittelerde.ch>
2011-02-28 19:57:08 +0100

    some updates

--- Tools/IMBAgit.sh
+++ Tools/IMBAgit.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+name=$(basename $0)
+
+function run {
+	echo -e "\n#########################################################"
+	echo -e "# $name:\t${@}"
+	echo -e "#########################################################\n"
+	${@}
+}
 
 run=true
 while $run;
 do
+	run=false
 
-	echo -e "\n### pulling from github:"
-	git pull
+	run git pull
+	run git push
 
-	echo -e "###pushing to github:"
-	git push
-
-
-	echo -e "\n### press enter for next update cycle or ctrl + c to exit"
+	echo -e "\n#########################################################"
+	echo -e "# press enter for next update cycle or ctrl + c to exit #\c"
 	read
+	sleep 1
+	run=true
+	echo -e "#########################################################\n\n"
+
 done

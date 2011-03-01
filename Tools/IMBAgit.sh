7d440e87487159baddf01c92a8dce70400d09e25
Marc Urben <aegnor@mittelerde.ch>
2011-03-01 02:01:31 +0100

    Baem updates

--- Tools/IMBAgit.sh
+++ Tools/IMBAgit.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+ #!/bin/bash
 
 name=$(basename $0)
 
 function run {
-	echo -e "\n#########################################################"
+        echo -e "\n"
+	echo -e "###############################################################################"
 	echo -e "# $name:\t${@}"
-	echo -e "#########################################################\n"
+	echo -e "###############################################################################"
 	${@}
+        echo -e "\n\n"
 }
 
 run=true
@@ -14,14 +16,33 @@ while $run;
 do
 	run=false
 
-	run git pull
-	run git push
-
-	echo -e "\n#########################################################"
-	echo -e "# press enter for next update cycle or ctrl + c to exit #\c"
-	read
+	echo -e "###############################################################################"
+	echo -e "#  git Commit  |  eXit  |  git Reset  |  git Status  |  Update (pull && push) #"
+	echo -e "###############################################################################"
+	read -n 1 -p "[c|x|r|s|u]:" input
+        case "$input" in
+        c|commit)
+            run=true
+            run git commit -a
+            ;;
+        x|exit)
+            echo "Exiting"
+            exit 0
+            ;;
+        r|reset)
+            run=true
+            run git reset
+            ;;
+        s|status)
+            run=true
+            run git status
+            ;;
+        u|update|*)
+            run=true
+            run git pull
+            run git push
+            ;;
+        esac
 	sleep 1
-	run=true
-	echo -e "#########################################################\n\n"
 
 done

813a385a142dddf587682c5b141957b25fe356da
Marc Urben <aegnor@mittelerde.ch>
2011-03-01 02:41:33 +0100

    added some ideas

--- ajax.php
+++ ajax.php
@@ -8,7 +8,26 @@ switch ($_POST["action"]) {
     case "messenger":
         include 'Ajax/Messenger.php';
         break;
-    
+
+    /**
+     * Some other examples
+    case "user":
+        include 'Ajax/User.php';
+        break;
+
+    case "role":
+        include 'Ajax/Role.php';
+        break;
+
+    case "chat":
+        include 'Ajax/Chat.php';
+        break;
+
+    case "event":
+        include 'Ajax/Event.php';
+        break;
+     */
+
     default:
         break;
 }

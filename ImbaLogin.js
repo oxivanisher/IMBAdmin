86d681592a81c39ecf26209b14f4bf7244838091
Marc Urben <aegnor@mittelerde.ch>
2011-03-03 01:47:48 +0100

    html content should now be nicer

--- ImbaLogin.js
+++ ImbaLogin.js
@@ -22,6 +22,37 @@ $(document).ready(function() {
 
 // Write ImbaOutput
 // TODO: Mach mich schoen!
+htmlContent = " \
+    <div id='test'></div> \
+    <select id='imbaUsers' size='10' ></select> \
+    <div class='imbaSsoLoginBorder ui-widget ui-widget-content ui-corner-all'></div> \
+    <div id='imbaSsoLogin'> \
+        <div id='imbaSsoLoginInner'> \
+            <img id='imbaSsoLogo' src='Images/guild_logo.png' alt='Guild Logo' /> \
+            <form id='imbaSsoLoginForm' action='ImbaAuth.php' method='get'> \
+                <input name='openid' type='text' /> \
+                <input id='imbaSsoOpenIdSubmit' type='submit' /> \
+            </form> \
+        </div> \
+    </div> \
+    <div id='imbaMessagesDialog' title='Alptr&ouml;im Messaging'> \
+        <div id='imbaMessages'> \
+            <ul></ul> \
+            <div id='imbaMessageTextDiv'> \
+                <form action='' method='post'> \
+                    <input id='imbaMessageText' type='text' /> \
+                    <input id='imbaMessageTextSubmit' type='submit' value='Send'/> \
+                </form> \
+            </div> \
+        </div> \
+    </div> \
+    ";
+
+document.write(htmlContent);
+
+/**
+* Backup for if the cosmetics did not work
+*
 document.writeln('<div id="test"></div>');
 document.writeln('<select id="imbaUsers" size="10" ></select>');
 document.writeln('<div class="imbaSsoLoginBorder ui-widget ui-widget-content ui-corner-all"></div>');
@@ -45,3 +76,4 @@ document.writeln('</form>');
 document.writeln('</div>');
 document.writeln('</div>');
 document.writeln('</div>');
+*/

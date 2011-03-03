69d8a68452ef9faafe6b75638827de4e72325958
Marc Urben <aegnor@mittelerde.ch>
2011-03-03 02:34:55 +0100

    try to recover from unknown error

--- ImbaLogin.js
+++ ImbaLogin.js
@@ -6,7 +6,7 @@
 // TODO: * für neue nachricht (New == 1)
 
 // Single point of Ajax entry   
-var ajaxEntry = "AJAX_ENTRY_REPLACE";
+var ajaxEntry = "ajax.php";
 
 // Test if user is online, if then show chat, else hide
 $(document).ready(function() {

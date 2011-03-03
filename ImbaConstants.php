ddffca4da3828339ed83c2c9458080a3c038c061
Marc Urben <aegnor@mittelerde.ch>
2011-03-03 02:30:23 +0100

    try to recover from an unknown problem

--- ImbaConstants.php
+++ ImbaConstants.php
@@ -11,51 +11,49 @@ require_once 'ImbaConfig.php';
  */
 class ImbaConstants extends ImbaConfig {
 
-        /**
-         * Files and Folders
-         */
-        public static $WEB_SITE_PATH = "/IMBAdmin";    
-        public static $WEB_OPENID_AUTH_PATH = "ImbaAuth.php";    
-        public static $WEB_ENTRY_INDEX_FILE = "index.html";
-        public static $WEB_AJAX_ENTRY_FILE = "ajax.php";
-        
-        /**
-         * Error Codes
-         */
-        // TODO: $ERROR_OPENID_Auth_OpenID_REDIRECT_FAILED -> $ERROR_Auth_OpenID_REDIRECT_FAILED
-        public static $ERROR_OPENID_Auth_OpenID_INVALID_URI = "Auth_OpenID_INVALID_URI";
-        public static $ERROR_OPENID_Auth_OpenID_REQUEST_FAILED = "Auth_OpenID_REQUEST_FAILED";
-        public static $ERROR_OPENID_Auth_OpenID_REDIRECT_FAILED = "Auth_OpenID_REDIRECT_FAILED";
-        public static $ERROR_OPENID_Auth_OpenID_FORM_FAILED = "Auth_OpenID_FORM_FAILED";
-        public static $ERROR_OPENID_Auth_OpenID_CANCEL = "Auth_OpenID_CANCEL";
-        public static $ERROR_OPENID_Auth_OpenID_FAILURE = "Auth_OpenID_FAILURE";
-        
-	/**
- 	* Database - Tables
- 	*/
-	public static $DATABASE_TABLES_SYS_PROFILES = "oom_openid_profiles";
-	public static $DATABASE_TABLES_SYS_REQUEST_LOG = "oom_openid_request_log";
-	public static $DATABASE_TABLES_SYS_SESSION = "oom_openid_session";
-	public static $DATABASE_TABLES_SYS_SETTINGS = "oom_openid_settings";
-	public static $DATABASE_TABLES_SYS_SYSTEMMESSAGES = "oom_openid_systemmessages";
-	public static $DATABASE_TABLES_SYS_ADMINUSERS = "oom_openid_usermanager";
-	public static $DATABASE_TABLES_SYS_FRONTEND_SAFE = "oom_openid_frontend_safe";
-	public static $DATABASE_TABLES_SYS_MULTIGAMING_GAMES = "oom_openid_multig_games";
-	public static $DATABASE_TABLES_SYS_USER_PROFILES = "oom_openid_user_profiles";
-
-	public static $DATABASE_TABLES_USR_MESSAGES = "oom_openid_messages";
-	public static $DATABASE_TABLES_USR_MULTIGAMING_NAMES = "oom_openid_multig_names";
-	public static $DATABASE_TABLES_USR_APPLICATIONS = "oom_openid_user_applications";
-	public static $DATABASE_TABLES_USR_XMPP = "oom_openid_xmpp";
-	public static $DATABASE_TABLES_USR_LASTONLINE = "oom_openid_lastonline";
-
-	public static $DATABASE_TABLES_WOW_ARMORY_ITEMCACHE = "oom_openid_armory_itemcache";
-	public static $DATABASE_TABLES_WOW_ARMORY_NAMES = "oom_openid_armory_names";
-	public static $DATABASE_TABLES_WOW_CHARACTERS = "oom_openid_characters";
-	public static $DATABASE_TABLES_WOW_ARMORY_CHARCACHE = "oom_openid_armory_charcache";
-
-	public static $DATABASE_TABLES_CHAT_CHATCHANNELS = "oom_openid_chatchannels";
-	public static $DATABASE_TABLES_CHAT_CHATMESSAGES = "oom_openid_chatmessages";
+    /**
+     * Files and Folders
+     */
+    public static $WEB_SITE_PATH = "/IMBAdmin";
+    public static $WEB_OPENID_AUTH_PATH = "ImbaAuth.php";
+    public static $WEB_ENTRY_INDEX_FILE = "index.html";
+    /*
+     * mimimi
+      public static $WEB_AJAX_ENTRY_FILE = "ajax.php";
+     */
+    /**
+     * Error Codes
+     */
+    // TODO: $ERROR_OPENID_Auth_OpenID_REDIRECT_FAILED -> $ERROR_Auth_OpenID_REDIRECT_FAILED
+    public static $ERROR_OPENID_Auth_OpenID_INVALID_URI = "Auth_OpenID_INVALID_URI";
+    public static $ERROR_OPENID_Auth_OpenID_REQUEST_FAILED = "Auth_OpenID_REQUEST_FAILED";
+    public static $ERROR_OPENID_Auth_OpenID_REDIRECT_FAILED = "Auth_OpenID_REDIRECT_FAILED";
+    public static $ERROR_OPENID_Auth_OpenID_FORM_FAILED = "Auth_OpenID_FORM_FAILED";
+    public static $ERROR_OPENID_Auth_OpenID_CANCEL = "Auth_OpenID_CANCEL";
+    public static $ERROR_OPENID_Auth_OpenID_FAILURE = "Auth_OpenID_FAILURE";
+    /**
+     * Database - Tables
+     */
+    public static $DATABASE_TABLES_SYS_PROFILES = "oom_openid_profiles";
+    public static $DATABASE_TABLES_SYS_REQUEST_LOG = "oom_openid_request_log";
+    public static $DATABASE_TABLES_SYS_SESSION = "oom_openid_session";
+    public static $DATABASE_TABLES_SYS_SETTINGS = "oom_openid_settings";
+    public static $DATABASE_TABLES_SYS_SYSTEMMESSAGES = "oom_openid_systemmessages";
+    public static $DATABASE_TABLES_SYS_ADMINUSERS = "oom_openid_usermanager";
+    public static $DATABASE_TABLES_SYS_FRONTEND_SAFE = "oom_openid_frontend_safe";
+    public static $DATABASE_TABLES_SYS_MULTIGAMING_GAMES = "oom_openid_multig_games";
+    public static $DATABASE_TABLES_SYS_USER_PROFILES = "oom_openid_user_profiles";
+    public static $DATABASE_TABLES_USR_MESSAGES = "oom_openid_messages";
+    public static $DATABASE_TABLES_USR_MULTIGAMING_NAMES = "oom_openid_multig_names";
+    public static $DATABASE_TABLES_USR_APPLICATIONS = "oom_openid_user_applications";
+    public static $DATABASE_TABLES_USR_XMPP = "oom_openid_xmpp";
+    public static $DATABASE_TABLES_USR_LASTONLINE = "oom_openid_lastonline";
+    public static $DATABASE_TABLES_WOW_ARMORY_ITEMCACHE = "oom_openid_armory_itemcache";
+    public static $DATABASE_TABLES_WOW_ARMORY_NAMES = "oom_openid_armory_names";
+    public static $DATABASE_TABLES_WOW_CHARACTERS = "oom_openid_characters";
+    public static $DATABASE_TABLES_WOW_ARMORY_CHARCACHE = "oom_openid_armory_charcache";
+    public static $DATABASE_TABLES_CHAT_CHATCHANNELS = "oom_openid_chatchannels";
+    public static $DATABASE_TABLES_CHAT_CHATMESSAGES = "oom_openid_chatmessages";
 
 
 }
--- ImbaLogin.js
+++ ImbaLogin.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 // Write ImbaOutput
 // TODO: Mach mich schoen!
-htmlContent = " \
+/*htmlContent = " \
     <div id='test'></div> \
     <select id='imbaUsers' size='10' ></select> \
     <div class='imbaSsoLoginBorder ui-widget ui-widget-content ui-corner-all'></div> \
@@ -47,12 +47,11 @@ htmlContent = " \
         </div> \
     </div> \
     ";
-
 document.write(htmlContent);
-
+*/
 /**
 * Backup for if the cosmetics did not work
-*
+*/
 document.writeln('<div id="test"></div>');
 document.writeln('<select id="imbaUsers" size="10" ></select>');
 document.writeln('<div class="imbaSsoLoginBorder ui-widget ui-widget-content ui-corner-all"></div>');
@@ -76,4 +75,3 @@ document.writeln('</form>');
 document.writeln('</div>');
 document.writeln('</div>');
 document.writeln('</div>');
-*/

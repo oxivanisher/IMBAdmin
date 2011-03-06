485d09070171731dc21004782f0346e97df2c182
Marc Urben <aegnor@mittelerde.ch>
2011-03-06 13:49:29 +0100

    Replaced ImbaLoader.php if/elseif construct with a nice and warm switch case statement. ImbaLoader.php?load=css or ImbaLoader.php?load=js

--- ImbaLoader.php
+++ ImbaLoader.php
@@ -3,19 +3,27 @@
 //require_once 'ImbaConstants.php';
 //echo ImbaConstants::$WEB_AJAX_ENTRY_FILE;
 
-if (isset($_GET["js"])) {
-    echo file_get_contents("Libs/jQuery/js/jquery-1.4.4.min.js") . "\n";
-    echo file_get_contents("Libs/jQuery/js/jquery-ui-1.8.10.custom.min.js") . "\n";
-    /*
-     * mimimi
-      echo str_replace("AJAX_ENTRY_REPLACE",
-      ImbaConstants::$WEB_AJAX_ENTRY_FILE,
-      file_get_contents("ImbaLogin.js") . "\n"); */
-    echo file_get_contents("ImbaLogin.js") . "\n";
-    echo file_get_contents("Controller/ImbaManagerMessage.js") . "\n";
-    echo file_get_contents("Controller/ImbaManagerOpenID.js") . "\n";
-} else if (isset($_GET["css"])) {
-//    echo file_get_contents("Libs/jQuery/css/ui-darkness/jquery-ui-1.8.10.custom.css") . "\n";
-    echo file_get_contents("ImbaLogin.css") . "\n";
+switch ($_GET["load"]) {
+    case "js":
+        echo file_get_contents("Libs/jQuery/js/jquery-1.4.4.min.js") . "\n";
+        echo file_get_contents("Libs/jQuery/js/jquery-ui-1.8.10.custom.min.js") . "\n";
+        /*
+         * mimimi
+          echo str_replace("AJAX_ENTRY_REPLACE",
+          ImbaConstants::$WEB_AJAX_ENTRY_FILE,
+          file_get_contents("ImbaLogin.js") . "\n"); */
+        echo file_get_contents("ImbaLogin.js") . "\n";
+        echo file_get_contents("Controller/ImbaManagerMessage.js") . "\n";
+        echo file_get_contents("Controller/ImbaManagerOpenID.js") . "\n";
+        break;
+
+    case "css":
+        echo file_get_contents("ImbaLogin.css") . "\n";
+        break;
+    
+    default:
+        echo "Please specify your request to ImbaLoader.php\n";
+        echo "ImbaLoader.php?load=js or ImbaLoader.php?load=css\n";
 }
+
 ?>

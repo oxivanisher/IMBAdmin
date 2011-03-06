2d864c28a5021ddbe4a1da8d781d83fc74b25017
Marc Urben <aegnor@mittelerde.ch>
2011-03-06 22:24:08 +0100

    nav dev

--- Ajax/Content/User.php
+++ Ajax/Content/User.php
@@ -13,21 +13,45 @@ require_once 'Controller/ImbaUserContext.php';
 $Navigation = new ImbaContentNavigation();
 $Navigation->addElement("overview", "Benutzer &Uuml;bersicht");
 $Navigation->addElement("myprofile", "Mein Profil Editieren");
+/**
+ * are we logged in?
+ */
+if (ImbaUserContext::getLoggedIn()) {
+    /**
+     * generate no content if only navigation is needed
+     */
+    if ($_POST["action"] != "navigation") {
+        /**
+         * Load the database
+         */
+        $managerDatabase = ImbaManagerDatabase::getInstance(ImbaConfig::$DATABASE_HOST, ImbaConfig::$DATABASE_DB, ImbaConfig::$DATABASE_USER, ImbaConfig::$DATABASE_PASS);
+        $managerUser = new ImbaManagerUser($managerDatabase);
 
-if ($_POST["action"] != "navigation") {
-    echo "<div id='ImbaErrorMsg'>DEBUG:<br /><pre>";
-    print_r($_POST);
-    echo "</pre></div>";
+        echo "<div id='ImbaErrorMsg'>DEBUG:<br /><pre>";
+        print_r($_POST);
+        echo "</pre></div>";
 
-    switch ($_POST["tabId"]) {
-        
-        case "#myprofile":
-            echo "MYPROFILE";
-            break;
+        switch ($_POST["tabId"]) {
 
-        default:
-            echo "OVERVIEW";
-            break;
+            case "#myprofile":
+                echo "MYPROFILE";
+                break;
+
+            default:
+                $users = $managerUser->selectAllUser(ImbaUserContext::getOpenIdUrl());
+                $result = array();
+                echo "<pre>";
+                foreach ($users as $user) {
+                    echo $user->getNickname() . " ";
+                }
+                echo "</pre>";
+
+                echo json_encode($result);
+
+                break;
+        }
     }
+} else {
+    echo "Not logged in";
 }
 ?>

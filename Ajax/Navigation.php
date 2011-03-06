551322c21fb45badc7bf8b8b5acc622aaad88415
Marc Urben <aegnor@mittelerde.ch>
2011-03-06 21:51:40 +0100

    navigation...

--- Ajax/Navigation.php
+++ Ajax/Navigation.php
@@ -5,14 +5,16 @@
  */
 $nav = array();
 
-if (file_exists("Ajax/Content/" . $_POST["module"] . ".php")) {
-    require_once "Ajax/Content/" . $_POST["module"] . ".php";
+$moduleFile = "Ajax/Content/" . $_POST["module"] . ".php";
+
+if (file_exists($moduleFile)) {
+    require_once $moduleFile;
 
     foreach ($Navigation->getElements() as $NavigationEntry) {
         array_push($nav, array("id" => $NavigationEntry, "name" => $Navigation->getElementName($NavigationEntry)));
     }
 } else {
-    array_push($nav, array("id" => "error", "name" => "Module not found!"));
+    array_push($nav, array("id" => "error", "name" => "Module not found (" . $moduleFile . ")!"));
 }
 echo json_encode($nav);
 ?>

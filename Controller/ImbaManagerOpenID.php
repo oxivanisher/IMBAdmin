cdd635ca862dd329de4e6075e8968ebfabfca70b
Marc Urben <aegnor@mittelerde.ch>
2011-03-06 16:43:47 +0100

    fixed getReturnTo and getTrustRoot URLs

--- Controller/ImbaManagerOpenID.php
+++ Controller/ImbaManagerOpenID.php
@@ -102,7 +102,7 @@ class ImbaManagerOpenID {
         // 
         // this sould be like that, if the webserver would be set up correctly
         // return sprintf("%s://%s:%s%s/?authDone=true", $this->getScheme(), $_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT'], dirname($_SERVER['PHP_SELF']));
-        return sprintf("%s://%s/IMBAdmin/ImbaAuth.php?authDone=true", $this->getScheme(), $_SERVER['SERVER_NAME']);
+        return sprintf("%s://%s/%s/ImbaAuth.php?authDone=true", $this->getScheme(), $_SERVER['SERVER_NAME'], dirname($_SERVER['PHP_SELF']));
     }
 
     /**
@@ -112,7 +112,7 @@ class ImbaManagerOpenID {
     protected function getTrustRoot() {
         // this sould be like that, if the webserver would be set up correctly
         // return sprintf("%s://%s:%s%s/", $this->getScheme(), $_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT'], dirname($_SERVER['PHP_SELF']));
-        return sprintf("%s://%s:%s/", $this->getScheme(), $_SERVER['SERVER_NAME'], dirname($_SERVER['PHP_SELF']));
+        return sprintf("%s://%s/%s/", $this->getScheme(), $_SERVER['SERVER_NAME'], dirname($_SERVER['PHP_SELF']));
     }
 
     /**

0cbc46171a8bfeba6eea7c7d8e672d658166e121
Marc Urben <aegnor@mittelerde.ch>
2011-03-07 03:12:30 +0100

    topnav dev

--- Ajax/Content/User.php
+++ Ajax/Content/User.php
@@ -1,13 +1,14 @@
 <?php
 
 // Extern Session start
-require_once 'ImbaConstants.php';
 require_once 'Model/ImbaUser.php';
-require_once 'Model/ImbaNavigation.php';
-require_once 'Controller/ImbaSharedFunctions.php';
+require_once 'ImbaConstants.php';
 require_once 'Controller/ImbaManagerDatabase.php';
 require_once 'Controller/ImbaManagerUser.php';
 require_once 'Controller/ImbaUserContext.php';
+
+require_once 'Model/ImbaNavigation.php';
+require_once 'Controller/ImbaSharedFunctions.php';
 
 $Navigation = new ImbaContentNavigation();
 $Navigation->addElement("overview", "Benutzer &Uuml;bersicht");

2d67137f9471dab417d118ef13c7e2d2fc1dd6be
Marc Urben <aegnor@mittelerde.ch>
2011-03-07 03:48:51 +0100

    topnav dev

--- Ajax/Content/User.php
+++ Ajax/Content/User.php
@@ -25,7 +25,7 @@ $Navigation->addElement("myprofile", "Mein Profil Editieren");
 /**
  * are we logged in?
  */
-print_r($_COOKIE);
+print_r($_SESSION);
 if (ImbaUserContext::getLoggedIn()) {
     echo "test2";
     /**

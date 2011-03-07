56de8a3d1f64bcfa738333cbacc5aa1b52209cce
Marc Urben <aegnor@mittelerde.ch>
2011-03-07 02:53:46 +0100

    topnav dev

--- Ajax/TopNavigation.php
+++ Ajax/TopNavigation.php
@@ -46,7 +46,7 @@ if (ImbaUserContext::getLoggedIn()) {
     foreach ($topNav->getElements() as $nav) {
         $navEntry = $topNav->getElementIdentifier($nav);
         array_push($smarty_navs, array(
-            "url" => ImbaSharedFunctions::genAjaxWebLink($_POST["mod_user"], "", $topNav->getElementIdentifier($Identifier)),
+            "url" => ImbaSharedFunctions::genAjaxWebLink($_POST["mod_user"], "", $navEntry),
             "name" => $topNav->getElementName($navEntry)
         ));
     }

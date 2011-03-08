92858bb7d7c65ae256e70b56687d45d52790160b
Marc Urben <aegnor@mittelerde.ch>
2011-03-08 17:56:31 +0100

    cosmetics

--- Ajax/Content/UserNavigation.php
+++ Ajax/Content/UserNavigation.php
@@ -6,7 +6,16 @@ require_once 'Model/ImbaNavigation.php';
  * Define Navigation
  */
 $Navigation = new ImbaContentNavigation();
+
+/**
+ * Set module name
+ */
 $Navigation->setName("Benuterverwaltung");
+
+/**
+ * Set tabs
+ */
 $Navigation->addElement("overview", "Benutzer &Uuml;bersicht");
 $Navigation->addElement("myprofile", "Mein Profil Editieren");
+
 ?>

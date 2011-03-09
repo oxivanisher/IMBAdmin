d4b82f5a202abfbd136bc9c0110bf52b3c743da1
Marc Urben <aegnor@mittelerde.ch>
2011-03-09 03:11:45 +0100

    topnav rewrite

--- ImbaLoader.php
+++ ImbaLoader.php
@@ -18,7 +18,65 @@ switch ($_GET["load"]) {
             /**
              * Generate TopNavigation
              */
-            include 'Ajax/TopNavigation.php';
+            session_start();
+
+            require_once 'Model/ImbaUser.php';
+            require_once 'ImbaConstants.php';
+            require_once 'Controller/ImbaManagerDatabase.php';
+            require_once 'Controller/ImbaManagerUser.php';
+            require_once 'Controller/ImbaUserContext.php';
+
+            require_once 'Model/ImbaNavigation.php';
+            require_once 'Controller/ImbaSharedFunctions.php';
+
+            /**
+             * FIXME: please add security to this file!
+             * FIXME: load top navigation from database
+             */
+            $topNav = new ImbaTopNavigation();
+            $topNav->addElement("blog", "Blog / News", "_top", "http://alptroeim.ch/blog/");
+            $topNav->addElement("forum", "Forum", "_top", "http://alptroeim.ch/forum/");
+
+            echo "\nhtmlContent = \" \\\n";
+            echo "<div id='imbaMenu'><ul class='topnav'>";
+
+            foreach ($topNav->getElements() as $nav) {
+                echo "<li><a href='" . $topNav->getElementUrl($nav) . "'>" . $topNav->getElementName($nav) . "</a></li>";
+            }
+
+            echo "<li>";
+            echo "<a id='imbaMenuImbAdmin' href='#'>Auf zum Atem</a>";
+            echo "<ul class='subnav'>";
+
+            if (ImbaUserContext::getLoggedIn()) {
+                $contentNav = new ImbaContentNavigation();
+
+                if ($handle = opendir('Ajax/Content/')) {
+                    $identifiers = array();
+                    while (false !== ($file = readdir($handle))) {
+                        if (strrpos($file, ".Navigation.php") > 0) {
+                            include 'Ajax/Content/' . $file;
+                            $modIdentifier = str_replace(".Navigation.php", "", $file);
+                            echo "<li><a href='#' onclick='javascript: loadImbaAdminModule(".'"'. $modIdentifier .'"'."");'>" . $Navigation->getName($nav) . "</a></li>";
+                            array_push($identifiers, $modIdentifier);
+                            $Navigation = null;
+                        }
+                    }
+                    closedir($handle);
+                }
+            } else {
+                echo "<li><a href=''>Registrieren</a></li>";
+            }
+
+            echo "</ul>";
+            echo "</li>";
+            echo "</li></ul></div>";
+            echo "\";\ndocument.write(htmlContent);\n\n";
+
+
+
+
+
             /**
              * Generate HTML construct (divs)
              */

e6bd84ad86568448a0172c3440c7a25809473936
Marc Urben <aegnor@mittelerde.ch>
2011-03-09 03:15:28 +0100

    topnav rewrite

--- ImbaLoader.php
+++ ImbaLoader.php
@@ -57,7 +57,7 @@ switch ($_GET["load"]) {
                         if (strrpos($file, ".Navigation.php") > 0) {
                             include 'Ajax/Content/' . $file;
                             $modIdentifier = str_replace(".Navigation.php", "", $file);
-                            echo "<li><a href='#' onclick='javascript: loadImbaAdminModule(".'"'. $modIdentifier .'"'.");'>" . $Navigation->getName($nav) . "</a></li>";
+                            echo "<li><a href='#' onclick='javascript: loadImbaAdminModule(\\\"". $modIdentifier ."\\\");'>" . $Navigation->getName($nav) . "</a></li>";
                             array_push($identifiers, $modIdentifier);
                             $Navigation = null;
                         }

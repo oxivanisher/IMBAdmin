68156da9bf36aacbf916c85b8684c82023595c47
Marc Urben <aegnor@mittelerde.ch>
2011-03-09 02:56:45 +0100

    topnav rewrite

--- Ajax/TopNavigation.php
+++ Ajax/TopNavigation.php
@@ -40,7 +40,7 @@ if (ImbaUserContext::getLoggedIn()) {
             if (strrpos($file, ".Navigation.php") > 0) {
                 include 'Ajax/Content/' . $file;
                 $modIdentifier = str_replace(".Navigation.php", "", $file);
-                echo "<li onclick='javascript: loadImbaAdminModule(" . $modIdentifier . ");'>" . $Navigation->getName($nav) . "</li>";
+                echo "<li><a href='#' onclick='javascript: loadImbaAdminModule('".$modIdentifier."')'>" . $Navigation->getName($nav) . "</a></li>";
                 array_push($identifiers, $modIdentifier);
                 $Navigation = null;
             }
@@ -55,5 +55,17 @@ echo "</ul>";
 echo "</li>";
 echo "</li></ul></div>";
 echo "\";\ndocument.write(htmlContent);\n\n";
+
+    echo '
+      function loadUserProfile(openid){
+      var data = {
+      action: "module",
+      module: "User",
+      tabId: "viewprofile",
+      openid: openid
+      };
+      loadImbaAdminTabContent(data);
+      }
+    ';
 
 ?>    

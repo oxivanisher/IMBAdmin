e6504ec726e7ca6869ba6d2e7e292979552978d3
Marc Urben <aegnor@mittelerde.ch>
2011-03-11 00:18:24 +0100

    navigation tuning

--- Ajax/Content/User.Navigation.php
+++ Ajax/Content/User.Navigation.php
@@ -1,12 +1,5 @@
 <?php
-session_start();
 
-require_once 'Model/ImbaUser.php';
-require_once 'ImbaConstants.php';
-require_once 'Controller/ImbaManagerDatabase.php';
-require_once 'Controller/ImbaUserContext.php';
-
-require_once 'Model/ImbaNavigation.php';
 
 /**
  * Show this navigation only if we are logged in

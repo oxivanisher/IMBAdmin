7cdde221209327a09e56aaae2e0614578413827a
Marc Urben <aegnor@mittelerde.ch>
2011-03-11 20:23:38 +0100

    last online

--- ImbaAuth.php
+++ ImbaAuth.php
@@ -109,6 +109,7 @@ if ($_GET["logout"] == true || $_POST["logout"] == true) {
      * - set cookie with logged in openid for autofill login box
      * - redirect back to page
      */
+    ImbaUser::setLastonline(time());
     setcookie("ImbaSsoLastLoginName", $_SESSION["IUC_openIdUrl"]);
     header("location: index.html");
 }

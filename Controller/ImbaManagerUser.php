60061f178a4f591102c55df9fdc6995c0856b21c
Marc Urben <aegnor@mittelerde.ch>
2011-03-11 21:25:13 +0100

    last online

--- Controller/ImbaManagerUser.php
+++ Controller/ImbaManagerUser.php
@@ -159,8 +159,8 @@ class ImbaManagerUser {
         $log = "setMeOnline: " . ImbaUserContext::getOpenIdUrl();
         ImbaSharedFunctions::writeToLog($log);
 
-        if ((ImbaUserContext::getLoggedIn() == true) && (!empty(ImbaUserContext::getOpenIdUrl()))) {
-//        if (ImbaUserContext::getOpenIdUrl()) {
+//        if ((ImbaUserContext::getLoggedIn() == true) && (!empty(ImbaUserContext::getOpenIdUrl()))) {
+        if (ImbaUserContext::getLoggedIn()) {
             $query = "UPDATE %s SET timestamp='%s' WHERE openid='%s';";
             //    $this->database->query($query, array(ImbaConstants::$DATABASE_TABLES_SYS_LASTONLINE, time(), ImbaUserContext::getOpenIdUrl()));
         }

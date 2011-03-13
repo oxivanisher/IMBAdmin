846ca4c192832195feb521bfd5a1d2a9fd7c72c7
Marc Urben <aegnor@mittelerde.ch>
2011-03-13 17:25:16 +0100

    welcome module

--- Ajax/Content/Welcome.php
+++ Ajax/Content/Welcome.php
@@ -1,8 +1,34 @@
 <?php
 
-echo "welcome";
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$contentNav = new ImbaContentNavigation();
+
+echo "<ul>";
+
+if ($handle = opendir('Ajax/Content/')) {
+    $identifiers = array();
+    while (false !== ($file = readdir($handle))) {
+        if (strrpos($file, ".Navigation.php") > 0) {
+            include 'Ajax/Content/' . $file;
+            if (ImbaUserContext::getUserRole() >= $Navigation->getMinUserRole()) {
+                $showMe = false;
+                if (ImbaUserContext::getLoggedIn() && $Navigation->getShowLoggedIn()) {
+                    $showMe = true;
+                } elseif ((!ImbaUserContext::getLoggedIn()) && $Navigation->getShowLoggedOff()) {
+                    $showMe = true;
+                }
+
+                if ($showMe) {
+                    $modIdentifier = str_replace(".Navigation.php", "", $file);
+                    echo "<li><a href='#' onclick='javascript: loadImbaAdminModule(\\\"" . $modIdentifier . "\\\");'>" . $Navigation->getName($nav) . "</a></li>";
+                    array_push($identifiers, $modIdentifier);
+                    $Navigation = null;
+                }
+            }
+        }
+    }
+    closedir($handle);
+}
+
+echo "</ul>";
+
 ?>

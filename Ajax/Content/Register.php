19ed6b1bc0dd2211bcc5894b5d8c8f38b67abcbd
Marc Urben <aegnor@mittelerde.ch>
2011-03-13 22:46:29 +0100

    working on welcome module

--- Ajax/Content/Register.php
+++ Ajax/Content/Register.php
@@ -35,6 +35,6 @@ if (ImbaUserContext::getLoggedIn()) {
 //            $smarty->assign('lastLogin', $user->getLastLogin());
 
 
-    $smarty->display('ImbaWebRegisterRegister.tpl');
+    $smarty->display('ImbaWebRegister.tpl');
 }
 ?>

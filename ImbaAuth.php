c1b986a6bd96112df6e14f77bb7234296acedf9f
Marc Urben <aegnor@mittelerde.ch>
2011-03-16 01:13:04 +0100

    last login cookie lifetime added

--- ImbaAuth.php
+++ ImbaAuth.php
@@ -110,7 +110,7 @@ if ($_GET["logout"] == true || $_POST["logout"] == true) {
      * - set cookie with logged in openid for autofill login box
      * - redirect back to page
      */
-    setcookie("ImbaSsoLastLoginName", $_SESSION["IUC_openIdUrl"]);
+    setcookie("ImbaSsoLastLoginName", $_SESSION["IUC_openIdUrl"], (time() + (60*60*24*30)));
     header("location: index.html");
 }
 

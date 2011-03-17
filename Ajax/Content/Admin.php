12b35ec2c6c32e8a73477d579222f82aa6ea31cd
Marc Urben <aegnor@mittelerde.ch>
2011-03-18 00:39:45 +0100

    added some suggestions to admin module

--- Ajax/Content/Admin.php
+++ Ajax/Content/Admin.php
@@ -1,7 +1,46 @@
 <?php
-echo "Admin";
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+// Extern Session start
+
+session_start();
+
+require_once 'Model/ImbaUser.php';
+require_once 'ImbaConstants.php';
+require_once 'Controller/ImbaManagerDatabase.php';
+require_once 'Controller/ImbaManagerUser.php';
+require_once 'Controller/ImbaManagerRole.php';
+require_once 'Controller/ImbaUserContext.php';
+require_once 'Controller/ImbaSharedFunctions.php';
+
+/**
+ * are we logged in?
  */
-?>
+if (ImbaUserContext::getLoggedIn()) {
+    /**
+     * create a new smarty object
+     */
+    $smarty = ImbaSharedFunctions::newSmarty();
+
+    /**
+     * Load the database
+     */
+    $managerDatabase = ImbaManagerDatabase::getInstance(ImbaConfig::$DATABASE_HOST, ImbaConfig::$DATABASE_DB, ImbaConfig::$DATABASE_USER, ImbaConfig::$DATABASE_PASS);
+    $managerUser = new ImbaManagerUser($managerDatabase);
+
+    switch ($_POST["request"]) {
+        case "role":
+            echo "role";
+            break;
+        case "settings":
+            echo "settings";
+            break;
+        case "debug":
+            echo "debug";
+            break;
+        default:
+            echo "log";
+            break;
+    }
+} else {
+    echo "Not logged in";
+}

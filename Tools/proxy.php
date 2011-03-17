5301224ab96708fdba4927d9e5bd64f2dc422373
Marc Urben <aegnor@mittelerde.ch>
2011-03-18 00:51:48 +0100

    proxy work

--- Tools/proxy.php
+++ Tools/proxy.php
@@ -3,12 +3,12 @@
 /*
  * This file should be kind of a fake proxy for overloading imbadmin oder other sites
  */
-echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"\n';
-echo '  http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">\n';
-echo '<html><head><title>Knowledge Base</title>\n';
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN"';
+echo '  http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">';
+echo '<html><head><title>Knowledge Base</title>';
 echo '<link type="text/css" href="http://alptroeim.ch/IMBAdmin/ImbaLoader.php?load=css" rel="Stylesheet" />';
 echo '<script type="text/javascript" src="http://alptroeim.ch/IMBAdmin/ImbaLoader.php?load=js"></script>';
 echo '</head><bod>';
-echo '<iframe src="http://braingoo.de" style="height:100%; width:100%; border:0px" frameborder="no" scrolling="auto"></iframe>';
+echo '<iframe src="http://braingoo.de" style="height:100%; width:100%; border:0px; overflow: auto;" frameborder="no" scrolling="auto"></iframe>';
 echo '</body></html>';
 ?>

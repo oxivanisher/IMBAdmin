6ec7b8b4ff34032cfd8a05941e7a91f0f8e3189f
Marc Urben <aegnor@mittelerde.ch>
2011-03-18 00:54:27 +0100

    proxy work

--- Tools/proxy.php
+++ Tools/proxy.php
@@ -1,5 +1,10 @@
 <?php
 
+if ($_GET["site"]) {
+    $site = $_GET["site"];
+} else {
+    $site = "http://braingoo.de";
+}
 /*
  * This file should be kind of a fake proxy for overloading imbadmin oder other sites
  */
@@ -9,6 +14,6 @@ echo '<html><head><title>Knowledge Base</title>';
 echo '<link type="text/css" href="http://alptroeim.ch/IMBAdmin/ImbaLoader.php?load=css" rel="Stylesheet" />';
 echo '<script type="text/javascript" src="http://alptroeim.ch/IMBAdmin/ImbaLoader.php?load=js"></script>';
 echo '</head><bod>';
-echo '<iframe src="http://braingoo.de" style="height:98%; width:100%; border:0px; overflow: auto;" frameborder="no" scrolling="auto"></iframe>';
+echo '<iframe src="'.$site.'" style="height:98%; width:100%; border:0px; overflow: auto;" frameborder="no" scrolling="auto"></iframe>';
 echo '</body></html>';
 ?>

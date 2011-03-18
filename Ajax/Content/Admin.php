04dc63e14e0e90505866916a69c1dff116aab520
Marc Urben <aegnor@mittelerde.ch>
2011-03-18 02:11:53 +0100

    admin gui user edit

--- Ajax/Content/Admin.php
+++ Ajax/Content/Admin.php
@@ -31,14 +31,100 @@ if (ImbaUserContext::getLoggedIn()) {
         case "role":
             echo "role";
             break;
+
         case "settings":
             echo "settings";
             break;
+
         case "log":
             echo "log";
             break;
+
+        case "updateuser":
+            $user = new ImbaUser();
+            $user = $managerUser->selectByOpenId($_POST["myProfileOpenId"]);
+            $user->setOpenId($_POST["myProfileOpenId"]);
+            $user->setSex($_POST["myProfileSex"]);
+            $user->setMotto($_POST["myProfileMotto"]);
+            $user->setRole($_POST["myProfileRole"]);
+            $user->setBirthmonth($_POST["myProfileBirthmonth"]);
+            $user->setBirthday($_POST["myProfileBirthday"]);
+            $user->setLastname($_POST["myProfileLastname"]);
+            $user->setFirstname($_POST["myProfileFirstname"]);
+            $user->setMotto($_POST["myProfileMotto"]);
+            $user->setUsertitle($_POST["myProfileUsertitle"]);
+            $user->setAvatar($_POST["myProfileAvatar"]);
+            $user->setWebsite($_POST["myProfileWebsite"]);
+            $user->setNickname($_POST["myProfileNickname"]);
+            $user->setEmail($_POST["myProfileEmail"]);
+            $user->setSkype($_POST["myProfileSkype"]);
+            $user->setIcq($_POST["myProfileIcq"]);
+            $user->setMsn($_POST["myProfileMsn"]);
+            $user->setSignature($_POST["myProfileSignature"]);
+            try {
+                $managerUser->update($user);
+                echo "Ok";
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+            break;
+
+        case "viewedituser":
+            $user = $managerUser->selectByOpenId(ImbaUserContext::getOpenIdUrl());
+
+            $smarty->assign('nickname', $user->getNickname());
+            $smarty->assign('lastname', $user->getLastname());
+            $smarty->assign('shortlastname', substr($user->getLastname(), 0, 1) . ".");
+            $smarty->assign('firstname', $user->getFirstname());
+            $smarty->assign('birthday', $user->getBirthday());
+            $smarty->assign('birthmonth', $user->getBirthmonth());
+            $smarty->assign('birthyear', $user->getBirthyear());
+            $smarty->assign('icq', $user->getIcq());
+            $smarty->assign('msn', $user->getMsn());
+            $smarty->assign('skype', $user->getSkype());
+            $smarty->assign('email', $user->getEmail());
+            $smarty->assign('website', $user->getWebsite());
+            $smarty->assign('motto', $user->getMotto());
+            $smarty->assign('usertitle', $user->getUsertitle());
+            $smarty->assign('avatar', $user->getAvatar());
+            $smarty->assign('signature', $user->getSignature());
+            $smarty->assign('openid', $user->getOpenid());
+            $smarty->assign('lastonline', ImbaSharedFunctions::getNiceAge($user->getLastonline()));
+
+            if (strtolower($user->getSex()) == "m") {
+                $smarty->assign('sex', 'Images/male.png');
+            } else if (strtolower($user->getSex()) == "w" || strtolower($user->getSex()) == "f") {
+                $smarty->assign('sex', 'Images/female.png');
+            } else {
+                $smarty->assign('sex', '');
+            }
+
+            $roleManager = new ImbaManagerUserRole($managerDatabase);
+            $role = $roleManager->selectById($user->getRole());
+
+            $smarty->assign('role', $role->getName());
+            $smarty->assign('roleIcon', $role->getIcon());
+
+
+            $smarty->display('ImbaWebAdminViewedituser.tpl');
+            break;
+
         default:
-            echo "user";
+            $smarty->assign('link', ImbaSharedFunctions::genAjaxWebLink($_POST["module"], "viewprofile", $_POST["User"]));
+            $users = $managerUser->selectAllUserButme(ImbaUserContext::getOpenIdUrl());
+
+            $smarty_users = array();
+            foreach ($users as $user) {
+                array_push($smarty_users, array(
+                    'nickname' => $user->getNickname(),
+                    'openid' => $user->getOpenID(),
+                    'lastonline' => ImbaSharedFunctions::getNiceAge($user->getLastonline()),
+                    'role' => ""
+                ));
+            }
+            $smarty->assign('susers', $smarty_users);
+
+            $smarty->display('ImbaWebAdminUserOverview.tpl');
             break;
     }
 } else {
---
+++ Templates/ImbaWebAdminViewedituser.tpl
@@ -0,0 +1,92 @@
+<script type="text/javascript">
+    $(document).ready(function() {
+        // User submits the ImbaWebUsersViewprofileForm
+        $("#ImbaWebUsersViewprofileFormSubmit").click(function(){
+            // submit the change
+            $.post(ajaxEntry, {
+                action: "module",
+                module: "User",
+                request: "updatemyprofile",
+                myProfileOpenId: "{$openid}",
+                myProfileMotto: $("#myProfileMotto").val(),
+                myProfileUsertitle: $("#myProfileUsertitle").val(),
+                myProfileAvatar: $("#myProfileAvatar").val(),
+                myProfileWebsite: $("#myProfileWebsite").val(),
+                myProfileNickname: $("#myProfileNickname").val(),
+                myProfileEmail: $("#myProfileEmail").val(),
+                myProfileSkype: $("#myProfileSkype").val(),
+                myProfileIcq: $("#myProfileIcq").val(),
+                myProfileMsn: $("#myProfileMsn").val(),
+                myProfileSignature: $("#myProfileSignature").val(),
+            }, function(response){
+                if (response != "Ok"){
+                    alert(response);
+                }
+            });
+            // TODO: Refresh from Database?
+            return false;
+        });
+    } );   
+</script>
+<form id="ImbaWebUsersViewprofileForm" action="post">
+    <table id="ImbaWebUsersViewprofileTable" style="cellspacing: 1px;">
+        <tbody>
+            <tr>
+                <td>
+                    <nobr>Aktuelles Motto:</nobr>
+                </td>
+                <td>
+                    <input id="myProfileMotto" type="text" name="motto" value="{$motto}" />
+                </td>
+                <td rowspan="8">
+                    <ul>
+                        <li><i>Um deinen Namen das Geschlecht oder dein Geburtstag zu &auml;ndern, kontaktiere bitte einen Administrator.</i></li>
+                        <li><i>Die Emailadresse wird ausschliesslich gebraucht um mit dir Kontakt aufzunehmen.</i></li>
+                        <li><i>Dein Nachname wird f&uuml;r alle anderen auf einen Buchstaben gek&uuml;rzt ({$firstname} {$shortlastname}).</i></li>
+                    </ul>
+                </td>
+                </tr>
+
+                <tr>
+                    <td>Titel:</td>
+                    <td><input id="myProfileUsertitle" type="text" name="usertitle" value="{$usertitle}" /></td>
+                </tr>
+                <tr>
+                    <td>Avatar URL:</td>
+                    <td><input id="myProfileAvatar" type="text" name="avatar" value="{$avatar}" /></td>
+                </tr>
+                <tr>
+                    <td>Webseite:</td>
+                    <td><input id="myProfileWebsite" type="text" name="website" value="{$website}" /></td>
+                </tr>
+                <tr>
+                    <td>Nickname:</td>
+                    <td><input id="myProfileNickname" type="text" name="nickname" value="{$nickname}" /></td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><input id="myProfileEmail" type="text" name="email" value="{$email}" /></td>
+                </tr>
+                <tr>
+                    <td>Skype:</td>
+                    <td><input id="myProfileSkype" type="text" name="skype" value="{$skype}" /></td>
+                </tr>
+                <tr>
+                    <td>ICQ:</td>
+                    <td><input id="myProfileIcq" type="text" name="icq" value="{$icq}" /></td>
+                </tr>
+                <tr>
+                    <td>MSN:</td>
+                    <td><input id="myProfileMsn" type="text" name="msn" value="{$msn}" /></td>
+                </tr>
+                <tr>
+                    <td>Signatur:</td>
+                    <td colspan="2"><textarea id="myProfileSignature" name="signature" rows="4" cols="50">{$signature}</textarea></td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td colspan="2"><input id="ImbaWebUsersViewprofileFormSubmit" type="submit" value="Speichern" /></td>
+                </tr>
+        </tbody>
+    </table>
+</form>

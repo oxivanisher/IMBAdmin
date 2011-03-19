3964e34151415810f0f6a2ccfd06b715d1f757c3
Marc Urben <aegnor@mittelerde.ch>
2011-03-19 21:45:52 +0100

    added tableedit for jquery

--- ImbaLoader.php
+++ ImbaLoader.php
@@ -13,7 +13,7 @@ switch ($_GET["load"]) {
         echo file_get_contents("Libs/jQuery/js/jquery-1.4.4.min.js") . "\n" . "\n";
         echo file_get_contents("Libs/jQuery/js/jquery-ui-1.8.10.custom.min.js") . "\n";
         echo file_get_contents("Libs/DataTables/media/js/jquery.dataTables.min.js") . "\n";
-        echo file_get_contents("Libs/EditTable/jquery.uitableedit.js") . "\n";
+        echo file_get_contents("Libs/TableEdit/jquery.uitableedit.js") . "\n";
         echo file_get_contents("Libs/jgrowl/jquery.jgrowl_compressed.js") . "\n";
 
         /**

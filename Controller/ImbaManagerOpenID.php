d4a027fdc2f87e63e52e87f983903ac9eb8dacac
Marc Urben <aegnor@mittelerde.ch>
2011-03-20 04:37:35 +0100

    more admin module work

--- Controller/ImbaManagerOpenID.php
+++ Controller/ImbaManagerOpenID.php
@@ -217,7 +217,7 @@ class ImbaManagerOpenID {
             if ($currentUser->getRole() != null) {
                 ImbaUserContext::setLoggedIn(true);
                 ImbaUserContext::setOpenIdUrl($esc_identity);
-                ImbaUserContext::setUserRole($currentUser->getRole()->getId());
+                ImbaUserContext::setUserRole($currentUser->getRole()->getRole());
                 $userManager->setMeOnline();
                 $this->afterLoginIsDone();
             } else {

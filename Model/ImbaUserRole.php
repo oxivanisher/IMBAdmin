2d8689383c748c3959d9073d95c065476612be34
Marc Urben <aegnor@mittelerde.ch>
2011-03-20 04:51:07 +0100

    more admin module work

--- Model/ImbaUserRole.php
+++ Model/ImbaUserRole.php
@@ -11,6 +11,7 @@ class ImbaUserRole extends ImbaBase {
     /**
      * Fields
      */
+    protected $id;
     protected $handle;
     protected $role;
     protected $name;
@@ -67,6 +68,14 @@ class ImbaUserRole extends ImbaBase {
 
     public function setIcon($icon) {
         $this->icon = $icon;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
     }
 
 }

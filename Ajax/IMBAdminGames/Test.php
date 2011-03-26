46ad287f69fd9a217092e2d9c73134405886f7d6
Marc Urben <aegnor@mittelerde.ch>
2011-03-26 03:29:06 +0100

    imbagame test

--- Ajax/IMBAdminGames/Test.php
+++ Ajax/IMBAdminGames/Test.php
@@ -1,5 +1,41 @@
 <?php
 
-echo "Test";
+session_start();
 
+require_once 'Model/ImbaUser.php';
+require_once 'ImbaConstants.php';
+require_once 'Controller/ImbaManagerUser.php';
+require_once 'Controller/ImbaManagerUserRole.php';
+require_once 'Controller/ImbaUserContext.php';
+require_once 'Controller/ImbaSharedFunctions.php';
+
+/**
+ * are we logged in?
+ */
+if (ImbaUserContext::getLoggedIn()) {
+    /**
+     * create a new smarty object
+     */
+    //$smarty = ImbaSharedFunctions::newSmarty();
+
+    /**
+     * Load the database
+     */
+    $managerUser = ImbaManagerUser::getInstance();
+
+
+    switch ($_POST["request"]) {
+
+        case "settings":
+            echo "settings";
+            break;
+
+        default:
+            echo "overview";
+    }
+    //$smarty->assign('test', true);
+    //$smarty->display('IMBAdminGames/WelcomeIndex.tpl');
+} else {
+    echo "Not logged in";
+}
 ?>

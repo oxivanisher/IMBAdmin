cae88a6d4c8a4ce4bc3e1af6832d963fef729962
unknown <escii@.(none)>
2011-04-17 03:14:46 +0200

    Added prototype Navigation for EVE and OOM, please replace this code with something more dynamic.

--- Imba.Navigation.php
+++ Imba.Navigation.php
@@ -7,6 +7,32 @@ $topNav = new ImbaTopNavigation();
 /**
  * Set up the top navigation
  */
+
+#PROTOTYPE CODE START - please replace with superior solution. esc
+/* 
 $topNav->addElement("blog", "News", "_top", "http://alptroeim.ch/blog/", "Zu Unserem Blog");
 $topNav->addElement("forum", "Forum", "_top", "http://alptroeim.ch/forum/", "Zu unserem Forum");
+*/
+switch ($_SERVER[SERVER_NAME]) {
+    	#OOM
+	case "www.oom.ch"||"oom.ch":
+                $topNav->addElement("blog", "Blog", "_top", "https://oom.ch/blog/", "OOM Blog");
+		$topNav->addElement("wiki", "Wiki", "_top", "https://oom.ch/wiki/", "OOM Wiki");
+		break;
+	#EVE 
+	case "b.oom.ch":
+		$topNav->addElement("forum", "Forum", "_top", "http://b.oom.ch/forum/", "the Dudez Forum");
+		$topNav->addElement("killboard", "Killboard", "_top", "http://b.oom.ch/kb/", "the Dudez Killboard");
+		break;
+	#WOW 
+	case "alptroeim.ch"||"www.alptroeim.ch":
+		echo "SERVER alptroeim.ch";
+		$topNav->addElement("blog", "News", "_top", "http://alptroeim.ch/blog/", "Zu Unserem Blog");
+		$topNav->addElement("forum", "Forum", "_top", "http://alptroeim.ch/forum/", "Zu unserem Forum");
+		break;	
+	default:
+		echo $_SERVER[SERVER_NAME];
+}
+
+
 ?>

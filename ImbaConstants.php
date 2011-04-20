1a35e979284497e70848c4ebc6994dd46ea96063
Marc Urben <aegnor@mittelerde.ch>
2011-04-21 01:55:26 +0200

    cleanup of old database table constants

--- ImbaConstants.php
+++ ImbaConstants.php
@@ -50,32 +50,35 @@ class ImbaConstants extends ImbaConfig {
      * Database - Tables
      */
     public static $DATABASE_TABLES_SYS_PORTALS = "oom_openid_portals";
-    public static $DATABASE_TABLES_SYS_PROFILES = "oom_openid_profiles";
-    public static $DATABASE_TABLES_SYS_REQUEST_LOG = "oom_openid_request_log";
-    public static $DATABASE_TABLES_SYS_SESSION = "oom_openid_session";
     public static $DATABASE_TABLES_SYS_SETTINGS = "oom_openid_settings";
+    public static $DATABASE_TABLES_SYS_PROFILES = "oom_openid_profiles";
+    public static $DATABASE_TABLES_SYS_USER_PROFILES = "oom_openid_user_profiles";
     public static $DATABASE_TABLES_SYS_SYSTEMMESSAGES = "oom_openid_systemmessages";
-    public static $DATABASE_TABLES_SYS_ADMINUSERS = "oom_openid_usermanager";
-    public static $DATABASE_TABLES_SYS_FRONTEND_SAFE = "oom_openid_frontend_safe";
     public static $DATABASE_TABLES_SYS_MULTIGAMING_GAMES = "oom_openid_multig_games";
     public static $DATABASE_TABLES_SYS_MULTIGAMING_GAMES_PROPERTIES = "oom_openid_multig_game_properties";
     public static $DATABASE_TABLES_SYS_MULTIGAMING_CATEGORIES = "oom_openid_multig_category";
     public static $DATABASE_TABLES_SYS_MULTIGAMING_INTERCEPT_GAMES_CATEGORY = "oom_openid_multig_int_games_cat";
     public static $DATABASE_TABLES_USR_MULTIGAMING_INTERCEPT_GAMES_PROPERTY = "oom_openid_multig_int_user_gameproperties";
-    public static $DATABASE_TABLES_SYS_USER_PROFILES = "oom_openid_user_profiles";
-    public static $DATABASE_TABLES_SYS_LASTONLINE = "oom_openid_lastonline";
-    public static $DATABASE_TABLES_USR_MESSAGES = "oom_openid_messages";
     public static $DATABASE_TABLES_USR_MULTIGAMING_NAMES = "oom_openid_multig_names";
-    public static $DATABASE_TABLES_USR_APPLICATIONS = "oom_openid_user_applications";
-    public static $DATABASE_TABLES_USR_XMPP = "oom_openid_xmpp";
-    public static $DATABASE_TABLES_USR_LASTONLINE = "oom_openid_lastonline";
-    public static $DATABASE_TABLES_WOW_ARMORY_ITEMCACHE = "oom_openid_armory_itemcache";
-    public static $DATABASE_TABLES_WOW_ARMORY_NAMES = "oom_openid_armory_names";
-    public static $DATABASE_TABLES_WOW_CHARACTERS = "oom_openid_characters";
-    public static $DATABASE_TABLES_WOW_ARMORY_CHARCACHE = "oom_openid_armory_charcache";
+    public static $DATABASE_TABLES_USR_MESSAGES = "oom_openid_messages";
     public static $DATABASE_TABLES_CHAT_CHATCHANNELS = "oom_openid_chatchannels";
     public static $DATABASE_TABLES_CHAT_CHATMESSAGES = "oom_openid_chatmessages";
     public static $DATABASE_TABLES_GAME_EVE_CHARS = "oom_game_eve_chars";
+    /**
+     * unused tables
+      public static $DATABASE_TABLES_SYS_REQUEST_LOG = "oom_openid_request_log";
+      public static $DATABASE_TABLES_SYS_SESSION = "oom_openid_session";
+      public static $DATABASE_TABLES_SYS_ADMINUSERS = "oom_openid_usermanager";
+      public static $DATABASE_TABLES_SYS_FRONTEND_SAFE = "oom_openid_frontend_safe";
+      public static $DATABASE_TABLES_SYS_LASTONLINE = "oom_openid_lastonline";
+      public static $DATABASE_TABLES_USR_APPLICATIONS = "oom_openid_user_applications";
+      public static $DATABASE_TABLES_USR_XMPP = "oom_openid_xmpp";
+      public static $DATABASE_TABLES_USR_LASTONLINE = "oom_openid_lastonline";
+      public static $DATABASE_TABLES_WOW_ARMORY_ITEMCACHE = "oom_openid_armory_itemcache";
+      public static $DATABASE_TABLES_WOW_ARMORY_NAMES = "oom_openid_armory_names";
+      public static $DATABASE_TABLES_WOW_CHARACTERS = "oom_openid_characters";
+      public static $DATABASE_TABLES_WOW_ARMORY_CHARCACHE = "oom_openid_armory_charcache";
+     */
     /**
      * Support for loading settings from database
      */

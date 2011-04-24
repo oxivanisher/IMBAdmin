6205c2f2c0294090e02b60323e0a38fc1d002cce
Marc Urben <aegnor@mittelerde.ch>
2011-04-24 03:45:03 +0200

    working on proxy

--- ImbaProxy.php
+++ ImbaProxy.php
@@ -113,7 +113,7 @@ if ($mimeType != "") {
 /**
  * Compute return
  */
-list($set['answerHeaders'], $set['answerContent']) = explode("\r\n\r\n", $set['answer'], 2);
+list($set['answerContent'], $set['answerHeaders']) = explode("\r\n\r\n", $set['answer'], 2);
 echo $set['answerContent']; exit;
 /**
  * generate output

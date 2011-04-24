07c6c865a9ee4bdf9a40b9b88a9fb3a7a4c8f361
Marc Urben <aegnor@mittelerde.ch>
2011-04-24 16:33:22 +0200

    working on proxy sessions

--- ImbaProxy.php
+++ ImbaProxy.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 require_once 'ImbaConstants.php';
 require_once 'Controller/ImbaSharedFunctions.php';
-print_r($GLOBALS);
+
 if ($_POST['PHPSESSID']) {
     session_name($_POST['PHPSESSID']);
 }

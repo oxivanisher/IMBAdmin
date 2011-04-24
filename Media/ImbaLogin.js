2744a6f298244f2999d4b5db855757499d0693ba
Marc Urben <aegnor@mittelerde.ch>
2011-04-24 03:20:57 +0200

    working on proxy

--- Media/ImbaLogin.js
+++ Media/ImbaLogin.js
@@ -302,8 +302,8 @@ function loadImbaPortal(id) {
         if ((response != "") && (tmpError == false)) {
             $.each($.parseJSON(response), function (name, icon) {
                 if (id != null) {
-                    $.jGrowl('<img src="' + icon + '" style="width: 24px; height: 24px;" align="middle" padding="3px"> <b>' + name + '</b>', {
-                        life: 200,
+                    $.jGrowl('<img src="' + icon + '" style="width: 24px; height: 24px;" align="top" padding="3px"> <b>' + name + '</b>', {
+                        life: 350,
                         header: 'Portal geladen:<br /><br />'
                     });
                 }

5825e84bf16c54470d8e4f2ff543a69fb6958b41
Marc Urben <aegnor@mittelerde.ch>
2011-04-25 06:29:07 +0200

    working on proxy sessions

--- Media/ImbaMessaging.js
+++ Media/ImbaMessaging.js
@@ -484,7 +484,6 @@ $(document).ready(function() {
                     type: "POST",
                     url: ajaxEntry,
                     dataType: "json",
-                    secSession: phpSessionID,
                     data: {
                         action: "user",
                         loaduser: "true",

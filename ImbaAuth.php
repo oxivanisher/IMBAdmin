8630ff5959ca23491a837b83ad9c111fc089e85d
Marc Urben <aegnor@mittelerde.ch>
2011-04-26 03:52:18 +0200

    bugfix roundhouse kick

--- ImbaAuth.php
+++ ImbaAuth.php
@@ -48,7 +48,7 @@ $managerUser = ImbaManagerUser::getInstance();
  * Helper for redirects
  */
 function redirectMe($url, $line = __LINE__) {
-    if (($line == 190) ||($line == 327) {
+    if (($line == 190) || ($line == 327)) {
         header("Location: " . $url);
     }
     echo $line . ": " . $url . "<br /><pre>";

d71fcacea6edcacecb99bf92f4b1781ed8ab730c
Marc Urben <aegnor@mittelerde.ch>
2011-04-30 19:23:16 +0200

    authrequests have now a own class, bugfixing

--- ImbaProxy.php
+++ ImbaProxy.php
@@ -53,9 +53,9 @@ if (!empty($_GET['imbaHash'])) {
     $_POST['openid_ns'] = $_GET['openid_ns'];
     $_POST['openid_op_endpoint'] = $_GET['openid_op_endpoint'];
     $_POST['openid_response_nonce'] = $_GET['openid_response_nonce'];
-    if ((strpos($_GET['openid_return_to'], "imbaHash") == false) && ($_GET['openid_return_to'] != "")) {
+    //if ((strpos($_GET['openid_return_to'], "imbaHash") == false) && ($_GET['openid_return_to'] != "")) {
         $_POST['openid_return_to'] = $_GET['openid_return_to'] . htmlspecialchars("&imbaHash=") . $_POST['imbaHash'];
-    }
+    //}
 
     $_POST['openid_sig'] = $_GET['openid_sig'];
     $_POST['openid_signed'] = $_GET['openid_signed'];

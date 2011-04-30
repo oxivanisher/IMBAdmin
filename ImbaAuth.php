e92896bf1c4a8c083523a23180f70278bf0e3754
Marc Urben <aegnor@mittelerde.ch>
2011-04-30 16:55:41 +0200

    authrequests have now a own class, bugfixing

--- ImbaAuth.php
+++ ImbaAuth.php
@@ -82,7 +82,7 @@ function redirectTo($line, $url, $message = "") {
      * Discover if we need to do the html redirect and make it so
      */
 //if (ImbaSharedFunctions::getDomain($_SERVER['HTTP_REFERER']) != ImbaSharedFunctions::getDomain($url)) {
-    if (headers_sent()) {
+    if (! headers_sent()) {
         $smarty = ImbaSharedFunctions::newSmarty();
         $smarty->assign("redirectUrl", $url);
         $smarty->assign("redirectDomain", $myDomain);

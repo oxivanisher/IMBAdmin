a37b07989eedfccbba4af7841c1bd7a425cffb2d
Marc Urben <aegnor@mittelerde.ch>
2011-05-01 01:34:44 +0200

    working on proxy

--- Libs/lightopenid/openid.php
+++ Libs/lightopenid/openid.php
@@ -665,7 +665,8 @@ class LightOpenID {
         }
 
         $params['openid.mode'] = 'check_authentication';
-        echo "s:".$server.", p:".$params; exit;
+        echo "s:".$server;
+        print_r($params); exit;
         $response = $this->request($server, 'POST', $params);
 
         return preg_match('/is_valid\s*:\s*true/i', $response);
